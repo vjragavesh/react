558a95967eca6db67a464e7635f6bc9d45c7c118
vijayaragavesh <vjragavesh@gmail.com>
2021-06-27 19:05:59 +0530

    first commit

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import './App.css';
+import Pageone from './Pageone'
+
+
+function App() {
+  return (
+    <div>
+      <Pageone/>
+    </div>
+  );
+}
+
+export default App;
---
+++ src/Pageone.js
@@ -0,0 +1,185 @@
+import { Accordion, TextareaAutosize, DialogTitle, DialogContent, DialogContentText, TextField, DialogActions, Divider, Dialog, Card, Badge, AccordionDetails, Typography, AccordionSummary, Grid, makeStyles, Button } from '@material-ui/core';
+import React, { useEffect } from 'react';
+import './App.css'
+
+const useStyles = makeStyles((theme) => ({
+    root: {
+        flexGrow: 1,
+    },
+    paperone: {
+        textAlign:"center",
+        padding: theme.spacing(2),
+        maxHeight: "460px",
+        border: '1px solid rgba(0, 0, 0, 190)',
+        marginTop: '-2%'
+    },
+    papertwo: {
+        padding: theme.spacing(2),
+        textAlign:"center",
+        marginTop: '-2%',
+        maxHeight: "460px",
+        border: '1px solid rgba(0, 0, 0, 190)'
+    },
+    badge: {
+        marginLeft: "1%",
+        padding: "3px",
+        borderRadius: '4px',
+        display: "absolute",
+        backgroundColor: "#187ad6",
+        color: "white"
+    },
+    control: {
+        padding: theme.spacing(4),
+    },
+    button: {
+        borderRadius: "50px",
+        color: "white",
+        backgroundColor: "#30a69a",
+        textTransform: "none",
+        marginTop:"10px"
+    },
+    img:{
+        height: "70px",
+        width:"60px"
+    }
+}));
+
+function Pageone() {
+
+    const classes = useStyles();
+    const [openpopup, setOpenpopup] = React.useState(false);
+    const [header, setHeader] = React.useState();
+    const [content, setContent] = React.useState();
+    const [buttonclick, setButtonclick] = React.useState(false);
+    const [listdetails, setListDetails] = React.useState([]);
+
+    const openPopup = () => {
+        setOpenpopup(true);
+    }
+
+    const closePopup = () => {
+        setOpenpopup(false);
+    }
+
+    const addDetails = () => {
+        setHeader(header);
+        setContent(content);
+        setButtonclick(true);
+    }
+
+    useEffect(() => {
+        const requestOptions = {
+            method: "GET",
+            headers: {
+                'content-type': 'application/json',
+                'Accept': 'application/json'
+            }
+        }
+
+        fetch("https://api.github.com/users", requestOptions)
+            .then(async res => {
+                console.log(res);
+                const data = await res.json();
+                console.log("Register response:", data);
+                setListDetails(data);
+                // listdetails.push(data);
+            })
+    },[1000])
+
+    const listview = () => {
+        console.log("enter into listview")
+        return (
+            <Accordion expanded style={{ backgroundColor: "#525659", color: "white" }}>
+                <AccordionSummary>
+                    <Typography>{header}</Typography>
+                </AccordionSummary><Divider />
+                <AccordionDetails>
+                    <Typography>
+                        {content}
+                    </Typography>
+                </AccordionDetails>
+            </Accordion>
+        );
+    }
+
+    console.log("header", header);
+    console.log("content", content);
+    return (
+        <div>
+            <Grid container className={classes.control}>
+                <Grid item xs style={{ padding: "10px" }}>
+                    <Badge className={classes.badge}> Group 1 </Badge>
+                    <Card className={classes.paperone}>
+                        <div style={{overflowY:'scroll',padding:'20px',height:'380px'}}>
+                        {listdetails.map((list, i) => (
+                            <Accordion expanded style={{ backgroundColor: "#525659", color: "white" }}>
+                                <AccordionSummary>
+                                    <Typography>{list.login}</Typography>
+                                </AccordionSummary><Divider />
+                                <AccordionDetails>
+                                   <img className={classes.img} src={list.avatar_url}/>
+                                </AccordionDetails>
+                            </Accordion>
+                        ))}
+                        </div>
+                        <Button className={classes.button}>Add</Button>
+                    </Card>
+                </Grid>
+
+                <Grid item xs style={{ padding: "10px" }}>
+                    <Badge className={classes.badge}> Group 2</Badge>
+                    <Card className={classes.papertwo}>
+                    <div style={{overflowY:'scroll',padding:'20px',height:'380px'}}>
+                        <Accordion expanded style={{ backgroundColor: "#525659", color: "white" }}>
+                            <AccordionSummary>
+                                <Typography>Header</Typography>
+                            </AccordionSummary><Divider />
+                            <AccordionDetails>
+                                <Typography>
+                                    Some quick example text to build on the card title and make up the bulk of the card's content.
+                                </Typography>
+                            </AccordionDetails>
+                        </Accordion>
+
+                        {buttonclick && listview()}
+                            </div>
+                        <Button className={classes.button} onClick={openPopup}>Add</Button>
+                    </Card>
+                </Grid>
+            </Grid>
+
+
+            <div>
+                <Dialog open={openpopup} onClose={closePopup} aria-labelledby="form-dialog-title">
+                    <DialogTitle id="form-dialog-title">Add details</DialogTitle>
+                    <DialogContent>
+                        <TextField
+                            autoFocus
+                            margin="dense"
+                            id="header"
+                            label="Header"
+                            type="email"
+                            onChange={e => { setHeader(e.target.value) }}
+                            fullWidth
+                        />
+                        <TextField
+                            label="Enter details here"
+                            rowsMin={4}
+                            onChange={e => { setContent(e.target.value) }}
+                            fullWidth
+                        />
+                    </DialogContent>
+                    <DialogActions>
+                        <Button color="primary" onClick={addDetails}>
+                            Add
+                        </Button>
+                    </DialogActions>
+                </Dialog>
+            </div>
+        </div>
+    )
+}
+
+
+
+export default Pageone;
